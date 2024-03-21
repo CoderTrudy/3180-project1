baf491cb345b3e3d36a673834c8a6d85b1289f6b
CoderTrudy <raeann.clarke@mymona.uwi.edu>
2024-03-20 23:27:13 -0500

    upload works

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, FileField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,InputRequired
 
 # Define form for adding a new property
 class PropertyForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
+    title = StringField('Property Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
-    bedrooms = StringField('Bedrooms', validators=[DataRequired()])
-    bathrooms = StringField('Bathrooms', validators=[DataRequired()])
+    bedrooms = StringField('No. of Rooms', validators=[DataRequired()])
+    bathrooms = StringField('No. of Bathrooms', validators=[DataRequired()])
     location = StringField('Location', validators=[DataRequired()])
     price = StringField('Price', validators=[DataRequired()])
-    type = SelectField('Property Type', choices=[('House', 'House'), ('Apartment', 'Apartment')], validators=[DataRequired()])
+    type = SelectField('Property Type', choices=[('House'), ( 'Apartment')], validators=[InputRequired()])
     photo = FileField('Photo', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file contains the routes for your application.
 """
 
-from app import app
+from app import app,db
 from flask import render_template, request, redirect, url_for
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
@@ -34,7 +34,8 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-@app.route("/properties/create")
+
+@app.route('/properties/create', methods=['GET', 'POST'])
 def create_properties():
     form = PropertyForm()
     if form.validate_on_submit():
@@ -58,11 +59,11 @@ def create_properties():
         )
         db.session.add(new_property)
         db.session.commit()
+        flash('Property created successfully!', 'success')
         return redirect(url_for('properties'))
 
+    flash_errors(form)
     return render_template('create_property.html', form=form)
-
-
 
 
 @app.route("/properties")
@@ -74,7 +75,7 @@ def properties():
 ###
 # The functions below should be applicable to all Flask apps.
 ###
-def view_properties(propertyid):
+def view_property(propertyid):
     property = Property.query.get_or_404(propertyid)
     return render_template('viewproperty.html', property=property)
 
@@ -110,3 +111,48 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+
+
+
+def upload_image():
+    if 'photo' not in request.files:
+        # No file part in the request
+        return None
+    
+    file = request.files['photo']
+    if file.filename == '':
+        # No selected file
+        return None
+    
+    if file:
+        # Ensure the existence of the upload directory
+        upload_dir = os.path.join(app.instance_path, 'uploads')
+        if not os.path.exists(upload_dir):
+            os.makedirs(upload_dir)
+        
+        # Save the file to the upload directory
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(upload_dir, filename)
+        file.save(filepath)
+        
+        # Return the filename or filepath, or any other necessary data
+        return filename  # or return filepath, or any other data as needed
+
+# Example usage:
+@app.route('/upload', methods=['POST'])
+def handle_upload():
+    uploaded_file = upload_image()
+    if uploaded_file:
+        return 'File uploaded successfully: ' + uploaded_file
+    else:
+        return 'Failed to upload file'
+
+
+UPLOAD_FOLDER = os.path.join(app.instance_path, 'uploads')
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+# Set the UPLOAD_FOLDER configuration
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
